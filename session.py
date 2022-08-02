52c70b77766e8597063c3627dd9d37ea5d74169c
Ernesto Crespo <ernesto@asistensi.com>
2022-08-02 14:35:52 -0400

    first commit

---
+++ session.py
@@ -0,0 +1,2 @@
+def sum_two_element(a,b):
+    return a+b 
---
+++ test_session.py
@@ -0,0 +1,20 @@
+import pytest
+from session import sum_two_element
+
+
+
+
+def test_add_possitive():
+    assert sum_two_element(1,2) == 3
+    
+def test_add_zero():
+    assert sum_two_element(1,0) == 1
+    
+def test_add_negative():
+    assert sum_two_element(4,-100)  == -96
+    
+    
+def test_add_string_expect_exception():
+    with pytest.raises(TypeError):
+        sum_two_element(4,'I do not have a number')
+    
